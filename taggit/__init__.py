59e62aef51107d9003b0aa74f52f920427d875a5
Jason Goldstein <jason@betheshoe.com>
2013-07-15 12:37:53 -0400

    Version bump.

--- taggit/__init__.py
+++ taggit/__init__.py
@@ -1,4 +1,1 @@
-__version_info__ = (0, 9, 2)
-__atl_version_info__ = (0, 1, 3)
-__version__ = '%s-atl-%s' % tuple(['.'.join(map(str, v))
-                                   for v in [__version_info__, __atl_version_info__]])
+__version__ = "{}.{}.{}".format(1,0,0)
